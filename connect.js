945c2e4851d17f244ab771943998909ecfa03b3c
syduan2 <syduan2@illinois.edu>
2015-02-28 16:31:13 -0600

    swaglife

--- connect.js
+++ connect.js
@@ -1,10 +1,10 @@
  SC.initialize({
           client_id: 'ada2ef9470a5b24e4858550357ca9a5b',
-          redirect_uri: 'http://external.codecademy.com/soundcloud.html'
-          
+          redirect_uri: 'http://jiajames.github.io/HackIllinois/callback.html'
+
         });
 
- 
+
 $(document).ready(function() {
   $('a.connect').click(function(e) {
     e.preventDefault();
@@ -15,4 +15,3 @@ $(document).ready(function() {
     });
   });
 });
-
